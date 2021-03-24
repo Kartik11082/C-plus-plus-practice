eca4f902e942be4995a25239418c3003ae01e079
Kartik <karkerakartik27@gmail.com>
2021-03-24 11:59:25 +0530

    Added 231A Team Codeforces problem

---
+++ team.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, ans = 0;
+    cin>>n;
+    while(n--)
+    {
+        int p, v, t;
+        cin>>p>>v>>t;
+        if(p+v+t >= 2)
+        {
+            ans++;
+        }
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
