9fe7202e7e84dbf0a5b1247449a60dc8b022c801
Kartik <karkerakartik27@gmail.com>
2021-03-31 20:18:22 +0530

    Added Petya abd Strings CodeForces 112A Solution

--- petya_and_strings.cpp
+++ petya_and_strings.cpp
@@ -1,33 +1,31 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
-    int i = 0, res1 = 0, res2 = 0;;
-    string str1;
-    string str2;
-    cin>>str1;
-    cin>>str2;
-    while(str1[i] != '\0' && str2[i] != '\0')
+    int i=0, r1 = 0, r2 = 0;
+    string s1, s2;
+    cin>>s1;
+    cin>>s2;
+    transform(s1.begin(), s1.end(), s1.begin(), :: tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), :: tolower);
+    int l = s1.length();
+    while(l--)
     {
-        if(int(str1[i]) == int(str2[i]) || (int(str1[i]) - int(str2[i]) == 32) || (int(str1[i]) - int(str2[i]) == -32));
-        else if(int(str1[i]) - int(str2[i]) < 32)
+        if(int(s1[i]) < int(s2[i]))
         {
-            res2++;
+            cout<<"-1";
+            return 0;
         }
-
-        else if(int(str1[i]) - int(str2[i]) > 32)
+        else if(int(s1[i]) > int(s2[i]))
         {
-            res1++;
+            cout<<"1";
+            return 0;
         }
         i++;
     }
-    if(res1 > res2)
-        cout<<1;
-    if(res1 < res2)
-        cout<<-1;
-    else
-        cout<<0;
+    cout<<"0";
     return 0;
 }
